2d454dec6176ac0c103f3873ed61657090e72b00
Daniel V <main@dvasquez4155.com>
2020-03-05 15:28:27 -0800

    Fully functional app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const q = require("./lib/questions");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,13 +9,14 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
-function init() {
-    const testArray = [
-        new Engineer('Daniel', '1', 'email', '10'),
-        new Manager('Daniel', '1', 'email', '10'),
-        new Intern('Daniel', '1', 'email', '10')
-    ];
-    const rendered = render(testArray);
+
+const employees = [];
+async function init() {
+    await askForEmployees();
+    const rendered = render(employees);
+    writeFile(rendered)
+}
+function writeFile(rendered) {
     if (!fs.existsSync(OUTPUT_DIR)){
         fs.mkdirSync(OUTPUT_DIR);
     }
@@ -25,26 +27,46 @@ function init() {
         console.log("The file was saved!");
     });
 }
+async function askForEmployees() {
+    while (true) {
+        const anotherEmployee = await inquirer.prompt(q.anotherEmployee);
+        if (!anotherEmployee.anotherEmployee) {
+            return;
+        }
+        const emp = {}
+        const name = await inquirer.prompt(q.employeeName);
+        emp.employeeName = name.employeeName
+        const info = await askForOthers(name.employeeName);
+        emp.employeeTitle = info.employeeTitle;
+        emp.employeeId = info.employeeId;
+        emp.employeeEmail = info.employeeEmail;
+        const contact = await inquirer.prompt(replaceReturn(replaceReturn(q.employeeContact, '${employeeName}', name.employeeName),'${employeeContact}',q.getEmployeeContact(emp.employeeTitle)))
+        emp.employeeContact = contact.employeeContact
+        employees.push(newEmployee(emp))
+    }
+}
+function newEmployee(emp) {
+    switch (emp.employeeTitle) {
+        case 'Manager':
+            return new Manager(emp.employeeName,emp.employeeId,emp.employeeEmail,emp.employeeContact)
+        case 'Engineer':
+            return new Engineer(emp.employeeName,emp.employeeId,emp.employeeEmail,emp.employeeContact)
+        case 'Intern':
+            return new Intern(emp.employeeName,emp.employeeId,emp.employeeEmail,emp.employeeContact)
+    }
+}
+function replaceReturn(question, string, replace) {
+    const q = question;
+    q.message = q.message.replace(string, replace)
+    return q;
+}
+async function askForOthers(name) {
+    const questions = [
+        replaceReturn(q.employeeTitle, '${employeeName}', name),
+        replaceReturn(q.employeeId, '${employeeName}', name),
+        replaceReturn(q.employeeEmail, '${employeeName}', name)
+    ]
+    return await inquirer.prompt(questions);
+}
+
 init();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-// ​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-// ​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// ​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-// ​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
